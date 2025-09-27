076fd4d1fde75dfab5a2334d675c5c5f66d67b95
JamesBRdasd <57716080+JamesBRdasd@users.noreply.github.com>
2025-09-27 10:54:35 +0300

    Main.java

--- Programming/Labs/Lab1/src/Main.java
+++ Programming/Labs/Lab1/src/Main.java
@@ -14,22 +14,29 @@ public class Main {
         double[][] w = new double[11][11];
         for (int i = 0; i < 11; i++) {
             for (int j = 0; j < 11; j++) {
-                double res = 0;
-                if (s[i] == 9) {
-                    res = Math.pow((3 + Math.atan((x[j] - 0.5) / 27.0)) / 2.0, 2);
-                }
-                else if (s[i] == 7 || s[i] == 13 || s[i] == 21 || s[i] == 23 || s[i] == 25) {
-                    res = Math.pow(1.0 / (2 * (1 - Math.cbrt(Math.cos(x[j])))), 2);
-                }
-                else {
-                    res = Math.log(Math.pow(Math.sin(Math.cbrt(Math.pow(4 * Math.pow((1 - x[j]) / (3.0 / 4.0), 3), 2))), 2));
-                }
-                w[i][j] = res;
+                w[i][j] = el(s[i],x[i]);
             }
         }
-        for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 11; j++) {
-                System.out.printf("%10.4f ", w[i][j]);
+        mat(w);
+    }
+    public static double el(long sel, float fel) {
+            double res = 0;
+            if (sel == 9) {
+                res = Math.pow((3 + Math.atan((fel - 0.5) / 27.0)) / 2.0, 2);
+            }
+            else if (sel == 7 || sel == 13 || sel == 21 || sel == 23 || sel == 25) {
+                res = Math.pow(1.0 / (2 * (1 - Math.cbrt(Math.cos(fel)))), 2);
+            }
+            else {
+                res = Math.log(Math.pow(Math.sin(Math.cbrt(Math.pow(4 * Math.pow((1 - fel) / (3.0 / 4.0), 3), 2))), 2));
+            }
+
+        return res;
+    }
+    public static void mat(double[][] matr) {
+        for (int i = 0; i < matr.length; i++) {
+            for (int j = 0; j < matr.length; j++) {
+                System.out.printf("%10.4f ", matr[i][j]);
             }
             System.out.println();
         }
